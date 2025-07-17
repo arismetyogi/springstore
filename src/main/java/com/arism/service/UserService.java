8e7f11a65a95d58611a6be1729e4084d91ae7ed6
arismetyogi <arismetyogi@gmail.com>
2025-07-17 12:27:35 +0700

    feat: create change password request, implement JWT auth/service

---
+++ src/main/java/com/arism/service/UserService.java
@@ -0,0 +1,40 @@
+package com.arism.service;
+
+import com.arism.exception.ResourceNotFoundException;
+import com.arism.model.User;
+import com.arism.repository.UserRepository;
+import com.arism.request.ChangePasswordRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+//    Register User
+    private User registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new IllegalStateException("User already exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(User.Role.USER);
+        return userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(()-> new ResourceNotFoundException("User not found"));
+    }
+
+    public void changePassword(String email, ChangePasswordRequest request) {
+        User user = getUserByEmail(email);
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Incorrect password");
+        }
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
+    }
+}
