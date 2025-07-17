43e52d06b6bb0e062967c289ddac9456db189120
arismetyogi <arismetyogi@gmail.com>
2025-07-17 10:06:54 +0700

    feat: added product, order and user repository

---
+++ src/main/java/com/arism/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.arism.repository;
+
+import com.arism.model.Comment;
+import com.arism.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
