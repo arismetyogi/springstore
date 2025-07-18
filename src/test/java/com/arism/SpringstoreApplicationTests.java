3ef8108271e970f3271110f89c645a5dadf0ca5a
arismetyogi <arismetyogi@gmail.com>
2025-07-18 17:04:02 +0700

    fix: remove contextLoad from main test class

--- src/test/java/com/arism/SpringstoreApplicationTests.java
+++ src/test/java/com/arism/SpringstoreApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringstoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
